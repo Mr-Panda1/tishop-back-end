5267a0692fbd82e37f0cc996d52df42dfb37836a
Darwensley <Kelvinsensei35@gmail.com>
2026-02-20 19:38:05 -0500

    Did not add PATCH in CORS while I was using patch request

--- src/server.js
+++ src/server.js
@@ -45,7 +45,7 @@ app.use(cors({
             callback(new Error('CORS not allowed'));
         }
     },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
