e25a9cf65f05a8d0e70273553ed9bb6fd1ec7ca9
Darwensley <Kelvinsensei35@gmail.com>
2026-03-18 18:53:52 -0400

    Fixed cookies for admin

--- src/routes/admins/auth.js
+++ src/routes/admins/auth.js
@@ -90,7 +90,7 @@ router.post('/admin/login', adminLoginLimiter, async (req, res) => {
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             maxAge: authData.session.expires_in * 1000,
             path: '/',
-            ...(process.env.NODE_ENV === 'production' && { domain: '.tishop.co' })
+            ...(process.env.NODE_ENV === 'production' && { domain: 'admin.tishop.co' })
         })
 
         if (authData.session.refresh_token) {
@@ -100,7 +100,7 @@ router.post('/admin/login', adminLoginLimiter, async (req, res) => {
                 sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
                 maxAge: 7 * 24 * 3600000,
                 path: '/',
-                ...(process.env.NODE_ENV === 'production' && { domain: '.tishop.co' })
+                ...(process.env.NODE_ENV === 'production' && { domain: 'admin.tishop.co' })
             });
         }
 
